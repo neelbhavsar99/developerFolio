81ce61bf3c96c84f0f98f0a438ef5802ab7ef39d
neelbhavsar99 <38928307+neelbhavsar99@users.noreply.github.com>
2022-11-14 14:12:01 +0000

    Deploying to gh-pages from  @ 27f78314509335fd793802739fea033e0675b561 🚀

---
+++ precache-manifest.1accfca9378360c6263d9de1df45b75e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98687fa958c2cbb4f64d14f210fb9e7e",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "c1172b64fd74851b522c",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "32a0ff72ef5f880e5370",
+    "url": "/developerFolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "7ec920bd18d5eafbb37b",
+    "url": "/developerFolio/static/css/main.bb0b3927.chunk.css"
+  },
+  {
+    "revision": "d7cc18ead3fb0224f022",
+    "url": "/developerFolio/static/js/2.56c106d6.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/developerFolio/static/js/2.56c106d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1172b64fd74851b522c",
+    "url": "/developerFolio/static/js/3.d24cc8f0.chunk.js"
+  },
+  {
+    "revision": "32a0ff72ef5f880e5370",
+    "url": "/developerFolio/static/js/4.0513155a.chunk.js"
+  },
+  {
+    "revision": "7ec920bd18d5eafbb37b",
+    "url": "/developerFolio/static/js/main.74ac00c8.chunk.js"
+  },
+  {
+    "revision": "747b3b2532a0b20ac8ec",
+    "url": "/developerFolio/static/js/runtime-main.16c5cb6f.js"
+  },
+  {
+    "revision": "6af2d6a0b030f174474aa7528ebca7cd",
+    "url": "/developerFolio/static/media/AI-Associate-for-Product-Managers.6af2d6a0.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7478873cb9f19907fe8b890a76795948",
+    "url": "/developerFolio/static/media/GWCaseComp.7478873c.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4e35d3fd3a12564aad020348df36aec1",
+    "url": "/developerFolio/static/media/NEELB_NVIDIA_CERT.4e35d3fd.png"
+  },
+  {
+    "revision": "2310593a50f42b23b3d373117524620d",
+    "url": "/developerFolio/static/media/RyersonEngComp.2310593a.png"
+  },
+  {
+    "revision": "a41e33286ccd725689f9050415c64956",
+    "url": "/developerFolio/static/media/amdLogo.a41e3328.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c4d08dd3140a6cac959e8509982d3ffa",
+    "url": "/developerFolio/static/media/ibmLogo.c4d08dd3.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95ddfe8e4b8fd337dab9022238c6ce66.js"
+  "/developerFolio/precache-manifest.1accfca9378360c6263d9de1df45b75e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
