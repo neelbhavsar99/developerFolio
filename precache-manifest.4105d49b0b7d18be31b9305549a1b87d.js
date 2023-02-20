6fc25448370e1a0d704be2d4da0c7fcb34796f8f
Neel Bhavsar <neel.bhavsar@ryerson.ca>
2023-02-20 13:45:04 -0500

    fixed reset

---
+++ precache-manifest.4105d49b0b7d18be31b9305549a1b87d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a399e9e900e3659f91378959cd6376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69dadb147f6d87ebb516",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "187ebbbfc3c6f98bdf89",
+    "url": "/static/css/main.69d71ecb.chunk.css"
+  },
+  {
+    "revision": "53d4234e859a234fcd16",
+    "url": "/static/js/2.275e7244.chunk.js"
+  },
+  {
+    "revision": "5fa7e26a4823c897a852accd31834d85",
+    "url": "/static/js/2.275e7244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69dadb147f6d87ebb516",
+    "url": "/static/js/3.2b4b9f41.chunk.js"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/static/js/4.8575ba78.chunk.js"
+  },
+  {
+    "revision": "187ebbbfc3c6f98bdf89",
+    "url": "/static/js/main.e44e9b6d.chunk.js"
+  },
+  {
+    "revision": "38e62496fe20d4175f97",
+    "url": "/static/js/runtime-main.0178d398.js"
+  },
+  {
+    "revision": "6af2d6a0b030f174474aa7528ebca7cd",
+    "url": "/static/media/AI-Associate-for-Product-Managers.6af2d6a0.png"
+  },
+  {
+    "revision": "25ede0a68143e8c852fd634ce70c16b3",
+    "url": "/static/media/AWS-Certified-Cloud-Practitioner.25ede0a6.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7478873cb9f19907fe8b890a76795948",
+    "url": "/static/media/GWCaseComp.7478873c.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4e35d3fd3a12564aad020348df36aec1",
+    "url": "/static/media/NEELB_NVIDIA_CERT.4e35d3fd.png"
+  },
+  {
+    "revision": "2310593a50f42b23b3d373117524620d",
+    "url": "/static/media/RyersonEngComp.2310593a.png"
+  },
+  {
+    "revision": "a41e33286ccd725689f9050415c64956",
+    "url": "/static/media/amdLogo.a41e3328.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c4d08dd3140a6cac959e8509982d3ffa",
+    "url": "/static/media/ibmLogo.c4d08dd3.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
